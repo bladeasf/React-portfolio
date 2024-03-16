8d6caf028cb6fc1aa9094c9500289e824198d5a6
bladeasf <125907788+bladeasf@users.noreply.github.com>
2024-03-16 06:11:29 -0400

    finished product

---
+++ src/assets/routes/AllProject.js
@@ -0,0 +1,58 @@
+const allProjects = [
+    {
+      title: 'Virtual Bookshelf',
+      img: require('../img/Project1.png'),
+      description:
+        'Virtual Bookshelf is a website designed to keep track of users favorite books along with a short description.',
+      websiteUrl: 'https://young-lake-53776-5ca5885c8205.herokuapp.com',
+      githubUrl: 'https://github.com/wchoi888/virtual_bookshelf',
+      techStack: ['Javascript','Bootsrap5'],
+    },
+    {
+      title: 'Concert Compass',
+      img: require('../img/Project2.png'),
+      description:
+        'Concert Compass is a website designed to help users look for events and concerts for their favorite artists in their area.',
+      websiteUrl: 'https://bladeasf.github.io/Project-1/',
+      githubUrl: 'https://github.com/bladeasf/Concert-Compass',
+      techStack: ['Javascript', 'Bulma'],
+    },
+    {
+      title: 'Architectural Designs',
+      img: require('../img/Project3.png'),
+      description:
+        'Architectural Designs page is a way for architects to view different designs and read about their backstory.',
+      websiteUrl: 'https://bladeasf.github.io/lesson5-homework/',
+      githubUrl: 'https://github.com/bladeasf/lesson5-homework',
+      techStack: ['HTML5','CSS3'],
+    },
+    {
+      title: 'Daily Planner',
+      img: require('../img/Project4.png'),
+      description:
+        'This application is a daily planner for users to save their daily to-do lists and remind themselves of what needs to be done for the day.',
+      websiteUrl: 'https://bladeasf.github.io/Daily-Planner-Calender/',
+      githubUrl: 'https://github.com/bladeasf/Daily-Planner-Calender',
+      techStack: ['Javascript'],
+    },
+    {
+      title: 'Programming Quiz',
+      img: require('../img/Project5.png'),
+      description:
+        'This is a timed quiz on JavaScript fundamentals that stores high scores. When a user clicks the start button, a timer starts and theyre presented with a question.',
+      websiteUrl: 'https://bladeasf.github.io/programming-quiz/',
+      githubUrl: 'https://github.com/bladeasf/programming-quiz',
+      techStack: ['Javascript'],
+    },
+    {
+      title: 'Note Taker',
+      img: require('../img/Project6.png'),
+      description:'The Note Taker application is a way for users to take notes which will be saved locally',
+      websiteUrl: 'https://bladeasf.github.io/note-taker/',
+      githubUrl: 'https://github.com/bladeasf/note-taker',
+      techStack: ['Javascript'],
+    },
+  
+  ];
+  
+  export default allProjects;
---
+++ src/assets/routes/Allimg.js
@@ -0,0 +1,18 @@
+import Project1 from "../img/Project1.png"
+import Project2 from "../img/Project2.png"
+import Project3 from "../img/Project3.png"
+import Project4 from "../img/Project4.png"
+import Project5 from "../img/Project5.png"
+import Project6 from "../img/Project6.png"
+
+
+ const ProjectImages = [
+    Project1,
+    Project2,
+    Project3,
+    Project4,
+    Project5, 
+    Project6,
+ ];
+
+ export default ProjectImages;
